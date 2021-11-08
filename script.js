00d6f3af0430d28fe824a83f6b1f35eb5e4ef9ec
Sarthak Ladhwe <sarthak.ladhwe156@gmail.com>
2021-11-08 12:58:39 +0530

    Updated fetch api using async

--- script.js
+++ script.js
@@ -7,22 +7,35 @@ const popup = document.getElementById('popup-container');
 
 const figureParts = document.querySelectorAll('.figure-part');
 
+let randomWord = "s";
+
 const correctLetters = ['a'];
 const wrongLetters = [];
 
+// function fetchRandomWord() {
+//     let fetchedWord;
+//     fetch('https://random-word-api.herokuapp.com/word?number=1')
+//         .then(res => res.json())
+//         .then(data => {
+//             fetchedWord = data[0];
+//             //console.log(randomWord);66
+//             //displayWord(randomWord);
+//     });
+//     return fetchedWord;
+// }
+
 fetchRandomWord();
 
-function fetchRandomWord() {
-    fetch('https://random-word-api.herokuapp.com/word?number=1')
-        .then(res => res.json())
-        .then(data => {
-            const randomWord = data[0];
-            //console.log(randomWord);
-            displayWord(randomWord);
-    });
+async function fetchRandomWord() {
+    const res = await fetch('https://random-word-api.herokuapp.com/word?number=1');
+    const data = await res.json();
+    //console.log(data)
+
+    randomWord = data[0];
+    displayWord();
 }
 
-function displayWord(randomWord) {
+function displayWord() {
     console.log(randomWord)
     wordEl.innerHTML = `
         ${randomWord
@@ -35,7 +48,17 @@ function displayWord(randomWord) {
             .join('')
         }
     `;
+
+    const innerWord = wordEl.innerText.replace(/\n/g,'');   // To remove the new line character
+    
+    if(innerWord === randomWord) {
+        finalMessage.innerText = 'Congratulations! You Won!';
+        popup.style.display = 'flex';
+    }
+    
 }
+
+
 
 
 
